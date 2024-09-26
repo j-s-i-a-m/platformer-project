82d115ada818e8b938a5b33691f797ffa4e66cad
johnjs01 <jsang0@ftstudent.org>
2024-09-26 12:32:41 -0400

    levelswitchnmore

---
+++ Scenes/NextLevel.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+const FILE_BEGIN = "res://Levels/Level"
+
+func _on_body_entered(body):
+		if body is Player:
+			var current_scene_file = get_tree().current_scene.scene_file_path
+			var next_level_number = current_scene_file.to_int() + 1
+			
+			var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
+			get_tree().change_scene_to_file(next_level_path)
+
